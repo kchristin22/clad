4a733ecd772d82c7a861acb7f9c56f76470193cf
kchristin <christinakoutsou22@gmail.com>
2025-02-09 13:55:40 +0200

    Update files from RSBench github repo

--- benchmark/RSBench/rsbench.cuh
+++ benchmark/RSBench/rsbench.cuh
@@ -9,6 +9,9 @@
 #include<assert.h>
 #include<cuda.h>
 #include <thrust/reduce.h>
+#include <thrust/count.h>
+#include <thrust/execution_policy.h>
+#include <thrust/sort.h>
 #include <chrono> 
 
 #define PI 3.14159265359
@@ -127,7 +130,7 @@ size_t get_mem_estimate( Input input );
 double get_time(void);
 
 // simulation.c
-void run_event_based_simulation(Input input, SimulationData data, SimulationData SD, unsigned long * vhash_result );
+void run_event_based_simulation(Input input, SimulationData data, unsigned long * vhash_result );
 void run_event_based_simulation_optimization_1(Input in, SimulationData GSD, unsigned long * vhash_result);
 __global__ void xs_lookup_kernel_baseline(Input in, SimulationData GSD );
 __device__ void calculate_macro_xs( double * macro_xs, int mat, double E, Input input, int * num_nucs, int * mats, int max_num_nucs, double * concs, int * n_windows, double * pseudo_K0Rs, Window * windows, Pole * poles, int max_num_windows, int max_num_poles );
