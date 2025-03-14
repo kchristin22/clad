ca5cfdebbf641a92151fd1350f99ed2d12493072
kchristin <christinakoutsou22@gmail.com>
2025-03-14 10:47:52 +0100

    Only keep necessary changes to ReverseModeVisitor

--- lib/Differentiator/ReverseModeVisitor.cpp
+++ lib/Differentiator/ReverseModeVisitor.cpp
@@ -3314,14 +3314,7 @@ Expr* ReverseModeVisitor::getStdInitListSizeExpr(const Expr* E) {
     assert(E && "must be provided");
     auto Type = utils::getNonConstType(E->getType(), m_Sema);
 
-    bool isOutVar = false;
-    if (auto arraySub = dyn_cast<ArraySubscriptExpr>(E))
-      if (auto declRef = dyn_cast<DeclRefExpr>(
-              arraySub->getBase()->IgnoreImpCasts()->IgnoreParens()))
-        if (m_Variables.find(declRef->getDecl()) != m_Variables.end())
-          isOutVar = true;
-
-    if (isInsideLoop && !isOutVar) {
+    if (isInsideLoop) {
       Expr* clone = Clone(E);
       if (moveToTape && E->getType()->isRecordType()) {
         llvm::SmallVector<Expr*, 1> args = {clone};
@@ -3355,14 +3348,6 @@ Expr* ReverseModeVisitor::getStdInitListSizeExpr(const Expr* E) {
     Stmt* Restore = nullptr;
     if (E->isModifiableLvalue(m_Context) == Expr::MLV_Valid)
       Restore = BuildOp(BO_Assign, Clone(E), Ref);
-
-    if (isInsideLoop) {
-      auto CladTape = MakeCladTapeFor(Clone(E), prefix);
-      Expr* Push = CladTape.Push;
-      Expr* Pop = CladTape.Pop;
-      auto* popAssign = BuildOp(BinaryOperatorKind::BO_Assign, Ref, Pop);
-      return {Push, popAssign};
-    }
 
     return {Store, Restore};
   }
